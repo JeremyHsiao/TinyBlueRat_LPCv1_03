7cbcfc845a798b0b3322258f9e7022a1e6d7cdd7
JeremyHsiao <jeremy.hsiao@outlook.com>
2013-08-14 09:32:49 +0800

    Add watchdog timer function but not tested yet.

---
+++ src/watchdog.h
@@ -0,0 +1,14 @@
+/*
+ * watchdog.h
+ *
+ *  Created on: 2013/8/13
+ *      Author: jeremy.hsiao
+ */
+
+#ifndef WATCHDOG_H_
+#define WATCHDOG_H_
+
+extern void ClearWatchdogTimer(void);
+
+
+#endif /* WATCHDOG_H_ */
